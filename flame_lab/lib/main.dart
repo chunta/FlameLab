2de2a0c3b25573f40ef8ecc9bdd4b881941fd3e6
rex <hi.chuntachen@gmail.com>
2024-04-27 15:50:03 +0800

    simple tap and jump the testing circle

--- flame_lab/lib/main.dart
+++ flame_lab/lib/main.dart
@@ -1,6 +1,8 @@
 import 'package:flame/components.dart';
+import 'package:flame/events.dart';
 import 'package:flutter/material.dart';
 import 'package:flame/game.dart';
+import 'package:logger/logger.dart';
 
 void main() {
   runApp(
@@ -8,8 +10,15 @@ void main() {
   );
 }
 
-class MyGame extends FlameGame {
+class MyGame extends FlameGame with TapCallbacks {
   late MyComponent myComponent;
+
+  var tapCount = 0;
+
+  var logger = Logger(
+    printer: PrettyPrinter(),
+  );
+
   MyGame() {
     add(myComponent = MyComponent());
   }
@@ -17,40 +26,46 @@ class MyGame extends FlameGame {
   @override
   Color backgroundColor() => Colors.redAccent;
 
-  void myTestFunction() async {
-    await Future.delayed(const Duration(seconds: 1));
-    remove(myComponent);
+  @override
+  void onTapDown(TapDownEvent event) {
+    logger.d('onTapDown $tapCount');
+    tapCount++;
+    myComponent.jump();
+    super.onTapDown(event);
   }
 }
 
-class MyComponent extends Component {
+class MyComponent extends PositionComponent {
   double speed = 30.0;
-  Vector2 position = Vector2(100, 100);
-
+  final _velocity = Vector2(0, 20.0);
+  final _gravity = 900.0;
+  final _jumpSpeed = 350.0;
   @override
   void onMount() {
     super.onMount();
+    position = Vector2(100, 0);
   }
 
   @override
   void update(double dt) {
     super.update(dt);
-    position = Vector2(position.x, 
-    position.y + dt * speed);
+    position += _velocity * dt;
+    _velocity.y += _gravity * dt;
   }
 
   @override
   void render(Canvas canvas) {
     super.render(canvas);
-    canvas.drawCircle(position.toOffset(),
-    15,
-    Paint()..color = Colors.yellow
-    );
+    canvas.drawCircle(position.toOffset(), 15, Paint()..color = Colors.yellow);
   }
 
   @override
   void onRemove() {
     print('onRemove');
     super.onRemove();
+  }
+
+  void jump() {
+    _velocity.y = -_jumpSpeed;
   }
 }
